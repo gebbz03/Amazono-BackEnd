248e03b90f713688d496c36b41a6fc7f243b1260
Gebb <gebb.freelancer@gmail.com>
2018-11-16 12:23:43 +0800

    v5-commit

---
+++ routes/seller.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const Product = require('../models/product');
+const aws = require('aws-sdk');
+
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const s3 = new aws.S3({
+    accessKeyId: "AKIAIIPE5NAM4PUW7NSA",
+    secretAccessKey: "ibKfzBa0FA2IJ+ur5q3MGs3WjIU7LXYrkHs3N3MM"
+});
+
+const faker=require('faker');
+
+const checkJWT = require('../middlewares/check-jwt');
+
+
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'amazonogebbz',
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+      },
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString())
+      }
+    })
+  })
+
+
+
+
+
+
+router.route('/products')
+    .get(checkJWT,(req,res,next) =>{
+       Product.find({owner:req.decoded.user._id})
+            .populate('owner')
+            .populate('category')
+            .exec((err,products) => {
+                if(products){
+                    res.json({
+                        success:true,
+                        message:"Products",
+                        products:products
+                    });
+                }
+            });
+    })
+
+    .post([checkJWT, upload.single('product_picture')], (req, res, next) => {
+        console.log(upload);
+        console.log(req.file);
+        let product = new Product();
+        product.owner = req.decoded.user._id;
+        product.category = req.body.categoryId;
+        product.title = req.body.title;
+        product.price = req.body.price;
+        product.description = req.body.description;
+        product.image = req.file.location;
+        product.save();
+        res.json({
+            success: true,
+            message: 'Successful'
+        });
+    });
+
+
+/* Faker API testing */
+router.get('/faker/test',(req,res,next) => {
+    for (i=0;i<20;i++){
+        let product=new Product();
+        product.category="5be70ec4cb35a982ac2fd0aa";
+        product.owner="5be6d1bf280fa1595ca384e0";
+        product.image=faker.image.cats();
+        product.title=faker.commerce.productName();
+        product.description=faker.lorem.words();
+        product.price=faker.commerce.price();
+        product.save();
+
+    }
+
+    res.json({
+        message:"Successfully added"
+    });
+
+});
+ 
+module.exports = router;
